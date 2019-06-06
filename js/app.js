ff3e21a0568fb2b89fcebc6dff56594681afd629
alan <alanduarte90@gmail.com>
2019-06-06 17:29:55 -0300

    Delete useless files

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 // "use strict"
 import '../css/app.scss'
 const el = document.createElement('span');
-
+const attr = document.createAttribute("class");
+attr.value = "span-el"
+el.setAttributeNode(attr)
 // let add = (function () {
   // var counter = 0;
   // return function () {counter += 1; return counter}
